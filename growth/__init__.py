9f41369aba43469a197e95fcc6fb929a234918f6
Griffin Chure <gchure@caltech.edu>
2021-01-25 09:42:04 -0800

    Initial commit of repository structure

---
+++ growth/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top level package for my PhD code"""
+
+
+# from . import mcmc
+from . import viz
+
+__author__ = """Griffin Chure"""
+__email__ = """griffinchure@gmail.com"""
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from os import path
+
+__version__ = "0.0.001"
+
+setup(
+    name="growth",
+    version=__version__,
+    description="",
+    license="BSD",
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    author="Griffin Chure",
+    author_email="griffinchure@gmail.com",
+)
