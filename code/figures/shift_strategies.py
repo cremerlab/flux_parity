a3e37e3c96198b5082970d953ed732afd78ba5d8
Griffin Chure <gchure@caltech.edu>
2021-08-04 17:48:40 -0700

    Update before reworking simulation code

--- code/figures/shift_strategies.py
+++ code/figures/shift_strategies.py
@@ -18,12 +18,12 @@ gamma_max = 9.65
 Kd = (20 * 1E6 * 110) / (0.15E-12 * 6.022E23)
 
 # Define the shift (either up- or down-shift)
-nu_init = 1 
+nu_init = 10 
 nu_shift = 10 
 time_shift = 0 # Time point at which the shift occurs.
  
 # Set the initial conditions
-M0 = 0.1 * OD_CONV
+M0 = 0.04 * OD_CONV
 phiR_init = growth.model.phi_R_optimal_allocation(gamma_max, nu_init, Kd, phi_O=0)
 cAA_init = growth.model.sstRNA_balance(nu_init, 1- phiR_init, gamma_max, phiR_init, Kd)
 gamma_init = gamma_max * (cAA_init / (cAA_init + Kd))
@@ -42,14 +42,14 @@ phiR_final_elong =  nu_shift / (gamma * (cAA_init + 1) + nu_shift)
 
 # Define the time ranges
 T_START = 0 
-T_END =  1 
-N_STEPS = T_END * 60 * 60 # steps of 1 s
-# N_STEPS = 300
+T_END =  2 
+# N_STEPS = 100 * T_END #T_END *  * 60 # steps of 1 s
+N_STEPS = 100 * T_END
 dt = (T_END - T_START) / N_STEPS
 time_range = np.linspace(T_START, T_END, N_STEPS)
 
 # Set the range of ribosomal allocation parameters to consider.
-phiR_range = np.linspace(0, 1, 500)
+phiR_range = np.linspace(0, 1, 3000)
 
 def integrate(params, t, gamma_max, nu_max, phiR, Kd=Kd):
     """
@@ -58,7 +58,7 @@ def integrate(params, t, gamma_max, nu_max, phiR, Kd=Kd):
     nu ≈ nu_max.
     """
     # Unpacking
-    M, Mr, Mp, cAA = params
+    Mr, Mp, cAA = params
     gamma_max, nu_max, phiR = args
 
     # Translational efficiency
@@ -68,19 +68,19 @@ def integrate(params, t, gamma_max, nu_max, phiR, Kd=Kd):
     dM_dt = gamma * Mr
             
     # Metabolism
-    dcAA_dt = (nu_max * Mp - (1 + cAA) * dM_dt) / M
- 
+    dcAA_dt = (nu_max * Mp - (1 + cAA) * dM_dt) / (Mr + Mp)
+
     # Allocation
     dMr_dt = phiR * dM_dt 
     dMp_dt = (1 - phiR) * dM_dt    
-    return np.array([dM_dt, dMr_dt, dMp_dt, dcAA_dt])
+    return np.array([dMr_dt, dMp_dt, dcAA_dt])
 
 #%% Scenario I: Instantaneous changing of phiR
 dfs = []
 for i, strat in enumerate(tqdm.tqdm(['constant', 'optimal', 'elongation'])):
     # Set the output vector and the initial conditions
-    out = np.zeros((4, len(time_range)))
-    out[:, 0] = [M0, Mr_init, Mp_init, cAA_init]
+    out = np.zeros((3, len(time_range)))
+    out[:, 0] = [Mr_init, Mp_init, cAA_init]
     nu = np.zeros_like(time_range)
     phi = np.zeros_like(time_range)
     nu[0] = nu_init
@@ -112,8 +112,8 @@ for i, strat in enumerate(tqdm.tqdm(['constant', 'optimal', 'elongation'])):
         phi[j]= _phi
 
     # Store everything as a dataframe
-    df = pd.DataFrame(out.T, columns=['M', 'Mr', 'Mp', 'cAA'])
-    df['rel_biomass'] = df['M'] / M0
+    df = pd.DataFrame(out.T, columns=['Mr', 'Mp', 'cAA'])
+    df['rel_biomass'] = (df['Mr'].values + df['Mp'].values )/ M0
     df['strategy'] = strat
     df['time'] = time_range
     df['nu'] = nu
@@ -156,11 +156,11 @@ layout = nu_plot & (M_plot  | cAA_plot | phiR_plot)
 layout
 #%% Scenario II: Dynamic changing of phiR
 dfs = []
-for i, strat in enumerate(tqdm.tqdm(['constant', 'optimal'])): 
+for i, strat in enumerate(tqdm.tqdm(['optimal'])): 
 
     # Set the output vector and the initial conditions
-    out = np.zeros((4, len(time_range)))
-    out[:, 0] = [M0, Mr_init, Mp_init, cAA_init]
+    out = np.zeros((3, len(time_range)))
+    out[:, 0] = [Mr_init, Mp_init, cAA_init]
     phi = np.zeros_like(time_range)
     nu = np.zeros_like(time_range)
     nu[0] = nu_init
@@ -173,7 +173,7 @@ for i, strat in enumerate(tqdm.tqdm(['constant', 'optimal'])):
         params = out[:, j-1]
 
         # If in the preshift condition, integrate given the initial steady state
-        if time_range[j] <= time_shift:
+        if time_range[j] < time_shift:
             args = (gamma_max, nu_init, phiR_init)
             _out = scipy.integrate.odeint(integrate, params, [0, dt], args=args) 
             out[:, j] = _out[-1]
@@ -194,53 +194,50 @@ for i, strat in enumerate(tqdm.tqdm(['constant', 'optimal'])):
             else:  
                 # Set a storage vector for the changes in biomass and cAA as 
                 # phiR is tuned.
-                biomass_diff = np.zeros_like(phiR_range)
+                biomass = np.zeros_like(phiR_range)
                 cAA = np.zeros_like(phiR_range)
 
                 # Sweep over values of phiR.
+                _outs = []
                 for k, _phi in enumerate(phiR_range):
                     args = (gamma_max, nu_shift, _phi)
                     _out = scipy.integrate.odeint(integrate, params, [0, dt], args=args)
 
                     # Save the change in the biomass and in the cAA.
                     _out = _out[-1]
-                    biomass_diff[k] = _out[0] - params[0]
+                    _outs.append(_out)
+                    biomass[k] = _out[0] + _out[1]
                     cAA[k] = _out[-1]
                 # Strategy 2: 'Optimal' allocation to maximize growth rate. 
                 # Choose the value of phiR which leads to the largest value of 
                 # dM_dt
                 if strat == 'optimal':
-                    ind = np.argmax(biomass_diff)
+                    ind = np.argmax(biomass)
                     _phi = phiR_range[ind]
-                    args = (gamma_max, nu_shift, _phi)
-                    _out = scipy.integrate.odeint(integrate, params, [0, dt], args=args) 
-                    out[:, j] =  _out[-1]
+                    out[:, j] =  _outs[ind]
                     phi[j] = _phi
                 
-                # Strategy 3: Restrain the ribosomal allocation such that 
-                # translation rate is maximized
+                #Strategy 3: Restrain the ribosomal allocation such that 
+                # translation rate is kept the same
 
-                # elif strat == 'elongation':
-                #     ind = np.where(abs(cAA - cAA_init) < 0.01)[0]
-                #     if len(ind) >= 1:
-                #         ind = ind[0]
-                #     elif len(ind) == 0:
-                #         if np.min(cAA) > cAA_init:
-                #             _phi = 1
-                #         else:
-                #             _phi = 0
-                #     else:
-                #         _phi = phiR_range[ind]
-                #     args= (gamma_max, nu_shift, _phi)
-                #     _out = scipy.integrate.odeint(integrate, params, [0, 2 * dt], args=args)[-1]
-                #     phi[j] = _phi
-                #     out[:, j] = _out
+                elif strat == 'elongation':
+                    ind = np.where(abs(cAA - cAA_init) < 0.001)[0]
+                    if len(ind) >= 1:
+                        ind = ind[0]
+                    elif len(ind) == 0:
+                        if np.min(cAA) > cAA_init:
+                            ind = len(phiR_range) -1
+                        else:
+                            ind = 0 
+                    _phi = phiR_range[ind]  
+                    phi[j] = _phi
+                    out[:, j] = _outs[ind]
 
     # Store everything as a dataframe
-    df = pd.DataFrame(out.T, columns=['M', 'Mr', 'Mp', 'cAA'])
+    df = pd.DataFrame(out.T, columns=['Mr', 'Mp', 'cAA'])
     df['phiR'] = phi
     df['nu'] = nu
-    df['rel_biomass'] = df['M'] / M0
+    df['rel_biomass'] = (df['Mr'].values + df['Mp'].values) / M0
     df['strategy'] = strat
     df['time'] = time_range
     dfs.append(df)
@@ -278,5 +275,7 @@ layout = nu_plot & (M_plot  | cAA_plot | phiR_plot)
 # altair_saver.save(layout, './nutrient_shift_dynamic_reallocation.pdf')
 layout
 
+
+# %%
 
 # %%
