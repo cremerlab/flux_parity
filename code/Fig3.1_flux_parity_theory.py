82f771780f322efb6a9ddcc613774055fc3ddbdf
gchure <griffinchure@gmail.com>
2023-02-01 22:10:02 -0800

    Reworking some figures for the response

--- code/Fig3.1_flux_parity_theory.py
+++ code/Fig3.1_flux_parity_theory.py
@@ -1,8 +1,8 @@
-#%%
-import numpy as np 
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import growth.viz 
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import growth.viz
 import growth.model
 import growth.integrate
 import scipy.integrate
@@ -17,18 +17,19 @@ colors, palette = growth.viz.matplotlib_style()
 sweep = pd.read_csv('../data/flux_parity_parameter_sweep.csv')
 sweep = sweep[sweep['nu_max'] == 4.5]
 # Generate the heatmaps
-phiRb_map = np.zeros((len(sweep['tau'].unique()), len(sweep['kappa_max'].unique())))
+phiRb_map = np.zeros(
+    (len(sweep['tau'].unique()), len(sweep['kappa_max'].unique())))
 i = 0
-for g, d in sweep.groupby(['tau']): 
+for g, d in sweep.groupby(['tau']):
     phiRb_map[i, :] = d['phiRb'].values
     i += 1
 
 # Compute the optimal growth rate and normalize the map
-opt_phiRb = growth.model.phiRb_optimal_allocation(const['gamma_max'], 
-                                                  sweep['nu_max'].values[0], 
-                                                  const['Kd_cpc'], 
+opt_phiRb = growth.model.phiRb_optimal_allocation(const['gamma_max'],
+                                                  sweep['nu_max'].values[0],
+                                                  const['Kd_cpc'],
                                                   const['phi_O'])
-norm_phiRb = phiRb_map / opt_phiRb
+norm_phiRb = phiRb_map - opt_phiRb
 
 # Create the mesh for positioning of contours and labels
 kappa_ind = np.arange(0, len(sweep['kappa_max'].unique()), 1)
@@ -36,53 +37,51 @@ tau_ind = np.arange(0, len(sweep['tau'].unique()), 1)
 X, Y = np.meshgrid(kappa_ind, tau_ind)
 
 
-# Compute over a range of nu max. 
+# Compute over a range of nu max.
 nu_range = np.linspace(0.5, 20, 200)
 FPM_phiRb_range = []
 for i, nu in enumerate(tqdm.tqdm(nu_range)):
-     args = {'nu_max':nu,
+    args = {'nu_max': nu,
             'gamma_max': const['gamma_max'],
             'Kd_TAA': const['Kd_TAA'],
             'Kd_TAA_star': const['Kd_TAA_star'],
             'tau': const['tau'],
             'kappa_max': const['kappa_max'],
             'phi_O': const['phi_O']}
-     out = growth.integrate.equilibrate_FPM(args)
-     FPM_phiRb_range.append(out[1]/out[0])
+    out = growth.integrate.equilibrate_FPM(args)
+    FPM_phiRb_range.append(out[1]/out[0])
 
 opt_phiRb_range = growth.model.phiRb_optimal_allocation(const['gamma_max'], nu_range,
-        const['Kd_cpc'], const['phi_O'])
+                                                        const['Kd_cpc'], const['phi_O'])
 
 
-
-
-#%%
+# %%
 # Generate the tent plots using the simple allocation
-nu_max = 4.5 
+nu_max = 4.5
 phiRb_range = np.linspace(0.001, 1 - const['phi_O'] - 0.001, 300)
-opt_lam = growth.model.steady_state_growth_rate(const['gamma_max'], opt_phiRb, 
-                                        nu_max, const['Kd_cpc'], const['phi_O'])
+opt_lam = growth.model.steady_state_growth_rate(const['gamma_max'], opt_phiRb,
+                                                nu_max, const['Kd_cpc'], const['phi_O'])
 flux_df = pd.DataFrame([])
 
 time = 100
-for i, phi in enumerate(tqdm.tqdm(phiRb_range)): 
+for i, phi in enumerate(tqdm.tqdm(phiRb_range)):
     # Equilibrate the flux-parity system
-    args = {'nu_max':nu_max,
+    args = {'nu_max': nu_max,
             'gamma_max': const['gamma_max'],
             'Kd_TAA': const['Kd_TAA'],
             'Kd_TAA_star': const['Kd_TAA_star'],
             'tau': const['tau'],
             'kappa_max': const['kappa_max'],
-            'dynamic_phiRb':{'phiRb': phi},
+            'dynamic_phiRb': {'phiRb': phi},
             'phi_O': const['phi_O'],
             'phiRb': phi}
     out = growth.integrate.equilibrate_FPM(args, max_iter=100)
     gamma = const['gamma_max'] * out[-1] / (out[-1] + const['Kd_TAA_star'])
     nu = nu_max * out[-2] / (out[-2] + const['Kd_TAA'])
     metab_flux = nu * out[2]/out[0]
-    trans_flux = gamma * out[1]/out[0] #(1 + out[-1] + out[-2])
-    _df = pd.DataFrame([[metab_flux, trans_flux]], 
-                      columns=['metab_flux', 'trans_flux'])
+    trans_flux = gamma * out[1]/out[0]  # (1 + out[-1] + out[-2])
+    _df = pd.DataFrame([[metab_flux, trans_flux]],
+                       columns=['metab_flux', 'trans_flux'])
     _df['TAA'] = out[-2]
     _df['TAA_star'] = out[-1]
     _df['balance'] = out[-1] / out[-2]
@@ -90,75 +89,72 @@ for i, phi in enumerate(tqdm.tqdm(phiRb_range)):
     flux_df = pd.concat([flux_df, _df], sort=False)
 
 # Find the flux parity optimum
-args = {'nu_max':nu_max,
-            'gamma_max': const['gamma_max'],
-            'Kd_TAA': const['Kd_TAA'],
-            'Kd_TAA_star': const['Kd_TAA_star'],
-            'tau': const['tau'],
-            'kappa_max': const['kappa_max'],
-            'phi_O': const['phi_O']}
+args = {'nu_max': nu_max,
+        'gamma_max': const['gamma_max'],
+        'Kd_TAA': const['Kd_TAA'],
+        'Kd_TAA_star': const['Kd_TAA_star'],
+        'tau': const['tau'],
+        'kappa_max': const['kappa_max'],
+        'phi_O': const['phi_O']}
 
 FPM_out = growth.integrate.equilibrate_FPM(args)
 FPM_phiRb = FPM_out[1]/FPM_out[0]
 
-#%%
+# %%
 fig, ax = plt.subplots(1, 3, figsize=(7, 2.25))
 # Format the axes
 ax[0].set_xlabel('allocation towards ribosomes\n$\phi_{Rb}$')
-ax[0].set_ylabel('$J_{tl}/\lambda_{max}$, $J_{Mb}/\lambda_{max}$'+'\nrelative flux ')
-ax[1].set_xlabel('maximum metabolic rate [hr$^{-1}$]\n' + r'$\nu_{max}$')
-ax[1].set_ylabel('$\phi_{Rb}^{(flux-parity)}/\phi_{Rb}^{(III)}$\nrelative ribosomal allocation')
-
+ax[0].set_ylabel(
+    '$J_{tl}/\lambda_{max}$, $J_{Mb}/\lambda_{max}$'+'\nrelative flux ')
 # Set axis limits
-ax[0].set_ylim([0, 1.05])
-ax[1].set_yticks([0.5, 0.75, 1, 1.25, 1.5])
-# ax[1].set_ylim([1E-4, 1E3])
+ax[1].set_xlabel('maximum metabolic rate [hr$^{-1}$]\n'+r'$\nu_{max}$')
+ax[1].set_ylabel(
+    r'$\phi_{Rb}^{(flux-parity)} - \phi_{Rb}^{(III)}$' + '\ndifference in allocation')
+# # Add appropriate heatmap ticks and labels.
+# inds = [[], []]
+# labs = [[], []]
+# for i, vals in enumerate([sweep['kappa_max'].unique(), sweep['tau'].unique()]):
+#     for j in range(np.log10(vals.min()).astype(int), np.log10(vals.max()).astype(int)+1):
+#         if j % 2 == 0:
+#             _ind = np.where(np.round(np.log10(vals), decimals=1) == j)
+#             inds[i].append(_ind[0][0])
+#             _exp = int(j)
+#             labs[i].append('10$^{%s}$' % _exp)
 
-# Heatmap
-ax[2].grid(False)
-ax[2].set_xlim([0, 100])
-ax[2].set_ylim([0, 100])
-ax[2].set_xlabel('transcription rate\n$\kappa_{max}$ [hr$^{-1}$]')
-ax[2].set_ylabel(r'$\tau$'+'\ncharging sensitivity')
-# Add appropriate heatmap ticks and labels.
-inds = [[], []]
-labs = [[], []]
-for i, vals in enumerate([sweep['kappa_max'].unique(), sweep['tau'].unique()]):
-    for j in range(np.log10(vals.min()).astype(int), np.log10(vals.max()).astype(int)+1):
-        if j%2==0:
-            _ind =  np.where(np.round(np.log10(vals), decimals=1) == j)
-            inds[i].append(_ind[0][0])  
-            _exp = int(j)
-            labs[i].append('10$^{%s}$' %_exp)
-
-ax[2].set_xticks(inds[0])
-ax[2].set_xticklabels(labs[0])
-ax[2].set_yticks(inds[1])
-ax[2].set_yticklabels(labs[1])
+# ax[1].set_xticks(inds[0])
+# ax[1].set_xticklabels(labs[0])
+# ax[1].set_yticks(inds[1])
+# ax[1].set_yticklabels(labs[1])
 
 # Make flux diagrams
-ax[0].plot(flux_df['phi_Rb'], flux_df['metab_flux'] / opt_lam, '-', color=colors['primary_purple'], lw=2.5)
-ax[0].plot(flux_df['phi_Rb'], flux_df['trans_flux'] / opt_lam, '-', color=colors['primary_gold'], lw=1)
-ax[0].vlines(FPM_phiRb, -0.01, 1.2, lw=1, color=colors['primary_red'], linestyle='--')
+ax[0].plot(flux_df['phi_Rb'], flux_df['metab_flux'] / opt_lam,
+           '-', color=colors['primary_purple'], lw=2.5)
+ax[0].plot(flux_df['phi_Rb'], flux_df['trans_flux'] /
+           opt_lam, '-', color=colors['primary_gold'], lw=1)
+ax[0].vlines(FPM_phiRb, -0.01, 1.2, lw=1,
+             color=colors['primary_red'], linestyle='--')
 
 # Plot tRNA species
-ax[1].plot(nu_range, FPM_phiRb_range/opt_phiRb_range, lw=1.5, color=colors['primary_red'])
+ax[1].plot(nu_range, FPM_phiRb_range-opt_phiRb_range,
+           lw=1.5, color=colors['primary_red'])
 
 # Plot the heatmap and contours
-ax[2].imshow(norm_phiRb, cmap='mako', origin='lower')
-cont_color = ['white', 'white', colors['primary_red'], 'white', 'white']
-locations = [(20, 90), (30, 75), (50, 70), (40, 60), (30, 45)]
-conts = ax[2].contour(X, Y, norm_phiRb, levels=[0.3, 0.7, 1, 1.5, 2], 
-                      colors=cont_color)
-ax[2].clabel(conts, conts.levels, inline=True, colors=cont_color, fontsize=6,
-            manual=locations, fmt=lambda x: f'{x}' + r' $\times$ $\phi_{Rb}^{(III)}$')
+# cmap = sns.diverging_palette(220, 20, as_cmap=True)
+# ax[1].imshow(norm_phiRb, cmap=cmap, origin='lower', vmin=-0.3, vmax=0.3)
+# cont_color = ['black', 'black',
+#               colors['primary_red'], 'black', 'black', 'black']
+# locations = [(20, 90), (30, 75), (50, 70), (40, 60), (30, 45), (10, 10)]
+# conts = ax[1].contour(X, Y, norm_phiRb, levels=[-0.1, -0.05, 0.0, 0.05, 0.1, 0.25],
+#                       colors=cont_color)
+# ax[1].clabel(conts, conts.levels, inline=True, colors=cont_color, fontsize=6,
+#              manual=locations)  # , fmt=)lambda x: f'{x}' + r' $\times$ $\phi_{Rb}^{(III)}$')
 
-
-ax[1].set_ylim(0.5, 1.5)
+ax[1].set_ylim([-0.1, 0.1])
+ax[1].set_yticks([-0.1, -0.05, 0, 0.05, 0.1])
+ax[2].axis('off')
 plt.tight_layout()
-plt.savefig('../figures/main_text/Fig3.1_FPM_plots.pdf', bbox_inches='tight')
+plt.savefig('../figures/main_text/Fig2.2_FPM_plots.pdf', bbox_inches='tight')
 # # %% Generate the heat maps
 # #%%
 
 # %%
-
--- code/Fig4_data_comparison.py
+++ code/Fig4_data_comparison.py
@@ -1,179 +1,190 @@
-#%%
-import numpy as np 
-import pandas as pd 
+# %%
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import growth.model 
+import growth.model
 import growth.integrate
-import growth.viz 
+import growth.viz
 import tqdm
 colors, _ = growth.viz.matplotlib_style()
 const = growth.model.load_constants()
 mapper = growth.viz.load_markercolors()
 
 # Load the data sets
-mass_frac = pd.read_csv('../data/main_figure_data/Fig4_ecoli_ribosomal_mass_fractions.csv')
-elong_rate = pd.read_csv('../data/main_figure_data/Fig4_ecoli_peptide_elongation_rates.csv')
+mass_frac = pd.read_csv(
+    '../data/main_figure_data/Fig4_ecoli_ribosomal_mass_fractions.csv')
+elong_rate = pd.read_csv(
+    '../data/main_figure_data/Fig4_ecoli_peptide_elongation_rates.csv')
 
 # Define the organism specific constants
 gamma_max = const['gamma_max']
-Kd_cpc =  const['Kd_cpc']
+Kd_cpc = const['Kd_cpc']
 nu_max = np.linspace(0.0001, 20, 500)
 phi_O = const['phi_O']
 
 # Compute the theory curves
 # Scenario I
 const_phiRb = 0.25 * np.ones_like(nu_max)
-const_lam = growth.model.steady_state_growth_rate(gamma_max, const_phiRb, nu_max, Kd_cpc, phi_O)
-const_gamma = growth.model.steady_state_gamma(gamma_max, const_phiRb, nu_max, Kd_cpc, phi_O) * 7459/3600
+const_lam = growth.model.steady_state_growth_rate(
+    gamma_max, const_phiRb, nu_max, Kd_cpc, phi_O)
+const_gamma = growth.model.steady_state_gamma(
+    gamma_max, const_phiRb, nu_max, Kd_cpc, phi_O) * 7459/3600
 
 # Scenario II
-cpc_phiRb = growth.model.phiRb_constant_translation(gamma_max, nu_max, 20, Kd_cpc, phi_O)
-cpc_lam = growth.model.steady_state_growth_rate(gamma_max, cpc_phiRb, nu_max, Kd_cpc, phi_O)
-cpc_gamma = growth.model.steady_state_gamma(gamma_max, cpc_phiRb, nu_max, Kd_cpc, phi_O) * 7459/3600
+cpc_phiRb = growth.model.phiRb_constant_translation(
+    gamma_max, nu_max, 20, Kd_cpc, phi_O)
+cpc_lam = growth.model.steady_state_growth_rate(
+    gamma_max, cpc_phiRb, nu_max, Kd_cpc, phi_O)
+cpc_gamma = growth.model.steady_state_gamma(
+    gamma_max, cpc_phiRb, nu_max, Kd_cpc, phi_O) * 7459/3600
 
 # Scenario III
-opt_phiRb = growth.model.phiRb_optimal_allocation(gamma_max,  nu_max, Kd_cpc, phi_O) 
-opt_lam = growth.model.steady_state_growth_rate(gamma_max,  opt_phiRb, nu_max, Kd_cpc, phi_O)
-opt_gamma = growth.model.steady_state_gamma(gamma_max, opt_phiRb,  nu_max, Kd_cpc, phi_O) * 7459/3600
+opt_phiRb = growth.model.phiRb_optimal_allocation(
+    gamma_max,  nu_max, Kd_cpc, phi_O)
+opt_lam = growth.model.steady_state_growth_rate(
+    gamma_max,  opt_phiRb, nu_max, Kd_cpc, phi_O)
+opt_gamma = growth.model.steady_state_gamma(
+    gamma_max, opt_phiRb,  nu_max, Kd_cpc, phi_O) * 7459/3600
 
 # Set up the figure canvas
-fig, ax = plt.subplots(1, 3, figsize=(6.5, 2.5), sharex=True)
-ax[0].axis('off')
+fig, ax = plt.subplots(1, 2, figsize=(7, 2.5), sharex=True)
 
 # Add labels
-ax[1].set(ylabel='$\phi_{Rb}$\nallocation to ribosomes',
-            xlabel='growth rate\n$\lambda$ [hr$^{-1}$]')
-ax[2].set(ylabel='$v_{tl}$ [AA / s]\ntranslation speed',
-
-             xlabel='growth rate\n$\lambda$ [hr$^{-1}$]')
+ax[0].set(ylabel='$M_{Rb}/M$\n ribosomal mass fraction',
+          xlabel='growth rate\n$\lambda$ [hr$^{-1}$]',
+          title='steady-state behavior')
+ax[1].set(ylabel='$v_{tl}$ [AA / s]\n translation speed',
+          xlabel='growth rate\n$\lambda$ [hr$^{-1}$]',
+          title='steady-state behavior')
 
 # Set ranges
-ax[1].set(ylim=[0, 0.3], xlim=[-0.05, 2])
-ax[2].set(ylim=[5, 20], xlim=[-0.05, 2])
+ax[0].set(ylim=[0, 0.3], xlim=[-0.05, 2.3])
+ax[1].set(ylim=[5, 20], xlim=[-0.05, 2.3])
 
 # Plot mass fraction
 counter = 100
 sources = []
-for g, d in mass_frac.groupby(['source']): 
+for g, d in mass_frac.groupby(['source']):
     if g not in sources:
         sources.append(g)
-    ax[1].plot(d['growth_rate_hr'], d['mass_fraction'], ms=4,  marker=mapper[g]['m'],
-                label='__nolegend__', alpha=0.75, linestyle='none',
-                markeredgecolor='k', markeredgewidth=0.25, color=mapper[g]['c'],
-                zorder=counter)
+    ax[0].plot(d['growth_rate_hr'], d['mass_fraction'], ms=4,  marker=mapper[g]['m'],
+               label='__nolegend__', alpha=0.75, linestyle='none',
+               markeredgecolor='k', markeredgewidth=0.25, color=mapper[g]['c'],
+               zorder=counter)
     counter += 1
-for g, d in elong_rate.groupby(['source']): 
+for g, d in elong_rate.groupby(['source']):
     if g not in sources:
         sources.append(g)
-    ax[2].plot(d['growth_rate_hr'], d['elongation_rate_aa_s'].values, marker=mapper[g]['m'],
-                 ms=4,  linestyle='none',  label='__nolegend__', color=mapper[g]['c'],
-                 markeredgewidth=0.25, markeredgecolor='k', alpha=0.75)
+    ax[1].plot(d['growth_rate_hr'], d['elongation_rate_aa_s'].values, marker=mapper[g]['m'],
+               ms=4,  linestyle='none',  label='__nolegend__', color=mapper[g]['c'],
+               markeredgewidth=0.25, markeredgecolor='k', alpha=0.75)
 
 # Theory curves for E. coli
-ax[1].plot(const_lam, const_phiRb, '-', color=colors['primary_black'], 
+ax[0].plot(const_lam, const_phiRb, '-', color=colors['primary_black'],
            label='(I) constant $\phi_{Rb}$', lw=1.5, zorder=1000)
-ax[1].plot(cpc_lam, cpc_phiRb, '-', color=colors['primary_green'], label='(II) constant $\gamma$', lw=1.5,
-            zorder=1000)
-ax[1].plot(opt_lam, opt_phiRb, '-', color=colors['primary_blue'], label='(III) optimal $\phi_{Rb}$', lw=1.5,
-            zorder=1000)
-ax[2].plot(const_lam, const_gamma, '-', color=colors['primary_black'], label='(I) constant $\phi_{Rb}$', lw=1.5,
-            zorder=1000)
-ax[2].plot(cpc_lam, cpc_gamma, '-', color=colors['primary_green'], label='(II) constant $\gamma$', lw=1.5,
-            zorder=1000)
-ax[2].plot(opt_lam, opt_gamma, '-', color=colors['primary_blue'], label='(III) optimal $\phi_{Rb}$', lw=1.5,
-            zorder=1000)
+ax[0].plot(cpc_lam, cpc_phiRb, '-', color=colors['primary_green'], label='(II) constant $\gamma$', lw=1.5,
+           zorder=1000)
+ax[0].plot(opt_lam, opt_phiRb, '-', color=colors['primary_blue'], label='(III) optimal $\phi_{Rb}$', lw=1.5,
+           zorder=1000)
+ax[1].plot(const_lam, const_gamma, '-', color=colors['primary_black'], label='(I) constant $\phi_{Rb}$', lw=1.5,
+           zorder=1000)
+ax[1].plot(cpc_lam, cpc_gamma, '-', color=colors['primary_green'], label='(II) constant $\gamma$', lw=1.5,
+           zorder=1000)
+ax[1].plot(opt_lam, opt_gamma, '-', color=colors['primary_blue'], label='(III) optimal $\phi_{Rb}$', lw=1.5,
+           zorder=1000)
 
-for g in sources:
-    ax[0].plot([], [], ms=4, marker=mapper[g]['m'], color=mapper[g]['c'], markeredgecolor='k',  
-                markeredgewidth=0.25, linestyle='none', label=g)
-plt.tight_layout()
-plt.savefig('../figures/main_text/plots/Fig4_simple_model_comparison.pdf', bbox_inches='tight')
-
-#%%
-# Generate the plots for the ppGpp model
-# Define the parameters
-nu_max = np.linspace(0.01, 20, 400)
-Kd_TAA = const['Kd_TAA'] 
-Kd_TAA_star = const['Kd_TAA_star'] 
-kappa_max = const['kappa_max']
-tau = const['tau']
-phi_O = const['phi_O']
-
-# #############################################################################  
-# Numerically compute the optimal scenario
-# #############################################################################  
-dt = 0.0001
-time_range = np.arange(0, 200, dt)
-ss_df = pd.DataFrame([])
-total_tRNA = 0.0004
-T_AA = total_tRNA / 2
-T_AA_star = total_tRNA / 2
-for i, nu in enumerate(tqdm.tqdm(nu_max)):
-    # Set the intitial state
-    _opt_phiRb = growth.model.phiRb_optimal_allocation(gamma_max,  nu, Kd_cpc) 
-    M0 = 1E9
-    phi_Mb = 1 -  _opt_phiRb
-    M_Rb = _opt_phiRb * M0
-    M_Mb = phi_Mb * M0
-    params = [M0, M_Rb, M_Mb, T_AA, T_AA_star]
-    args = {'gamma_max': gamma_max,
-            'nu_max':  nu,
-            'tau': tau,
-            'Kd_TAA': Kd_TAA,
-            'Kd_TAA_star':Kd_TAA_star,
-            'kappa_max': kappa_max,
-            'phi_O': phi_O}
-
-    # Integrate
-    _out = growth.integrate.equilibrate_FPM(args, tol=3, max_iter=100)
-
-    # Compute the final props
-    ratio = _out[-1] / _out[-2]
-    tRNA_abund = _out[-2] + _out[-1]
-    biomass = _out[0]
-    gamma = gamma_max * _out[-1]/ (_out[-1]+ Kd_TAA_star)
-
-    ss_df = ss_df.append({'phi_Rb': _out[1] / _out[0],
-                          'lam': gamma * (_out[1] /_out[0]),
-                          'gamma': gamma * 7459 / 3600,
-                          'balance': ratio,
-                          'tot_tRNA_abundance': tRNA_abund,
-                          'biomass': biomass,
-                          'tRNA_per_ribosome': (tRNA_abund * biomass) / (_out[1]/7459), 
-                          'nu_max': nu},
-                          ignore_index=True)
-# %%
-fig, ax = plt.subplots(1, 3, figsize=(6.5, 2.5))
-# Add labels
-ax[1].set(ylabel='$\phi_{Rb}$\nallocation to ribosomes',
-            xlabel='growth rate\n$\lambda$ [hr$^{-1}$]')
-ax[2].set(ylabel='$v_{tl}$ [AA / s]\ntranslation speed',
-             xlabel='growth rate\n$\lambda$ [hr$^{-1}$]')
-ax[0].axis('off')
-
-# Set ranges
-ax[1].set(ylim=[0, 0.3], xlim=[-0.05, 2])
-ax[2].set(ylim=[5, 20], xlim=[-0.05, 2])
-
-# Plot mass fraction
-sources = []
-for g, d in mass_frac.groupby(['source']): 
-    ax[1].plot(d['growth_rate_hr'], d['mass_fraction'], ms=4,  marker=mapper[g]['m'],
-                label='__nolegend__', alpha=0.75, linestyle='none',
-                markeredgecolor='k', markeredgewidth=0.25, color=mapper[g]['c'])
-
-
-for g, d in elong_rate.groupby(['source']):
-        ax[2].plot(d['growth_rate_hr'], d['elongation_rate_aa_s'].values, marker=mapper[g]['m'],
-                 ms=4,  linestyle='none',  label='__nolegend__', color=mapper[g]['c'],
-                 markeredgewidth=0.25, markeredgecolor='k', alpha=0.75)
-
-
-ax[1].plot(opt_lam, opt_phiRb, '-', color=colors['primary_blue'], lw=1.5)
-ax[1].plot(ss_df['lam'], ss_df['phi_Rb'], '--', color=colors['primary_red'], zorder=1000, lw=1.5)
-ax[2].plot(opt_lam, opt_gamma, '-', color=colors['primary_blue'], lw=1.5)
-ax[2].plot(ss_df['lam'], ss_df['gamma'], '--', color=colors['primary_red'], zorder=1000, lw=1.5)
-plt.tight_layout()
-plt.savefig('../figures/main_text/plots/Fig4_flux-parity_plots.pdf', bbox_inches='tight')
+# for g in sources:
+#     ax[0].plot([], [], ms=4, marker=mapper[g]['m'], color=mapper[g]['c'], markeredgecolor='k',
+#                 markeredgewidth=0.25, linestyle='none', label=g)
+# plt.tight_layout()
+plt.savefig('../figures/main_text/plots/Fig1.2_simple_model_comparison.pdf',
+            bbox_inches='tight')
 
 # %%
+# # Generate the plots for the ppGpp model
+# # Define the parameters
+# nu_max = np.linspace(0.01, 20, 400)
+# Kd_TAA = const['Kd_TAA']
+# Kd_TAA_star = const['Kd_TAA_star']
+# kappa_max = const['kappa_max']
+# tau = const['tau']
+# phi_O = const['phi_O']
+
+# # #############################################################################
+# # Numerically compute the optimal scenario
+# # #############################################################################
+# dt = 0.0001
+# time_range = np.arange(0, 200, dt)
+# ss_df = pd.DataFrame([])
+# total_tRNA = 0.0004
+# T_AA = total_tRNA / 2
+# T_AA_star = total_tRNA / 2
+# for i, nu in enumerate(tqdm.tqdm(nu_max)):
+#     # Set the intitial state
+#     _opt_phiRb = growth.model.phiRb_optimal_allocation(gamma_max,  nu, Kd_cpc)
+#     M0 = 1E9
+#     phi_Mb = 1 -  _opt_phiRb
+#     M_Rb = _opt_phiRb * M0
+#     M_Mb = phi_Mb * M0
+#     params = [M0, M_Rb, M_Mb, T_AA, T_AA_star]
+#     args = {'gamma_max': gamma_max,
+#             'nu_max':  nu,
+#             'tau': tau,
+#             'Kd_TAA': Kd_TAA,
+#             'Kd_TAA_star':Kd_TAA_star,
+#             'kappa_max': kappa_max,
+#             'phi_O': phi_O}
+
+#     # Integrate
+#     _out = growth.integrate.equilibrate_FPM(args, tol=3, max_iter=100)
+
+#     # Compute the final props
+#     ratio = _out[-1] / _out[-2]
+#     tRNA_abund = _out[-2] + _out[-1]
+#     biomass = _out[0]
+#     gamma = gamma_max * _out[-1]/ (_out[-1]+ Kd_TAA_star)
+
+#     ss_df = ss_df.append({'phi_Rb': _out[1] / _out[0],
+#                           'lam': gamma * (_out[1] /_out[0]),
+#                           'gamma': gamma * 7459 / 3600,
+#                           'balance': ratio,
+#                           'tot_tRNA_abundance': tRNA_abund,
+#                           'biomass': biomass,
+#                           'tRNA_per_ribosome': (tRNA_abund * biomass) / (_out[1]/7459),
+#                           'nu_max': nu},
+#                           ignore_index=True)
+# # %%
+# fig, ax = plt.subplots(2, 3, figsize=(6.5, 2.5))
+# # Add labels
+# ax[1].set(ylabel='$\phi_{Rb}$\nallocation to ribosomes',
+#             xlabel='growth rate\n$\lambda$ [hr$^{-1}$]')
+# ax[2].set(ylabel='$v_{tl}$ [AA / s]\ntranslation speed',
+#              xlabel='growth rate\n$\lambda$ [hr$^{-1}$]')
+# ax[0].axis('off')
+
+# # Set ranges
+# ax[1].set(ylim=[0, 0.3], xlim=[-0.05, 2])
+# ax[2].set(ylim=[5, 20], xlim=[-0.05, 2])
+
+# # Plot mass fraction
+# sources = []
+# for g, d in mass_frac.groupby(['source']):
+#     ax[1].plot(d['growth_rate_hr'], d['mass_fraction'], ms=4,  marker=mapper[g]['m'],
+#                 label='__nolegend__', alpha=0.75, linestyle='none',
+#                 markeredgecolor='k', markeredgewidth=0.25, color=mapper[g]['c'])
+
+
+# for g, d in elong_rate.groupby(['source']):
+#         ax[2].plot(d['growth_rate_hr'], d['elongation_rate_aa_s'].values, marker=mapper[g]['m'],
+#                  ms=4,  linestyle='none',  label='__nolegend__', color=mapper[g]['c'],
+#                  markeredgewidth=0.25, markeredgecolor='k', alpha=0.75)
+
+
+# ax[1].plot(opt_lam, opt_phiRb, '-', color=colors['primary_blue'], lw=1.5)
+# ax[1].plot(ss_df['lam'], ss_df['phi_Rb'], '--', color=colors['primary_red'], zorder=1000, lw=1.5)
+# ax[2].plot(opt_lam, opt_gamma, '-', color=colors['primary_blue'], lw=1.5)
+# ax[2].plot(ss_df['lam'], ss_df['gamma'], '--', color=colors['primary_red'], zorder=1000, lw=1.5)
+# plt.tight_layout()
+# plt.savefig('../figures/main_text/plots/Fig4_flux-parity_plots.pdf', bbox_inches='tight')
+
+# # %%
